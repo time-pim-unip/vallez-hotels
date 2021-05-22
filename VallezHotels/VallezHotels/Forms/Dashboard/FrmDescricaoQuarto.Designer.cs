655b20c78391ced25bb99487dffddde88b55eb5a
dcmunhoz <dc.munhoz@hotmail.com>
2021-05-21 21:35:37 -0300

    Tela de dashboard

---
+++ VallezHotels/VallezHotels/Forms/Dashboard/FrmDescricaoQuarto.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace VallezHotels
+{
+    partial class FrmDescricaoQuarto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumQuarto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumQuarto
+            // 
+            this.lblNumQuarto.AutoSize = true;
+            this.lblNumQuarto.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumQuarto.Location = new System.Drawing.Point(116, 26);
+            this.lblNumQuarto.Name = "lblNumQuarto";
+            this.lblNumQuarto.Size = new System.Drawing.Size(83, 30);
+            this.lblNumQuarto.TabIndex = 0;
+            this.lblNumQuarto.Text = "Quarto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hospede: Daniel Costa Munhoz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data Entrada: 01/01/2021";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Data Saída: 07/01/2021";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Check-out: 07/01/2021";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Check-in: 02/01/2021";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 30);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Andar: 1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(14, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Suite";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(14, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Situação: Disponivel";
+            // 
+            // FrmDescricaoQuarto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(296, 188);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNumQuarto);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmDescricaoQuarto";
+            this.Text = "FrmDescricaoQuarto";
+            this.Load += new System.EventHandler(this.FrmDescricaoQuarto_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FrmDescricaoQuarto_MouseClick);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.FrmDescricaoQuarto_MouseDoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumQuarto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ VallezHotels/VallezHotels/Forms/Dashboard/FrmDescricaoQuarto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VallezHotels.Source.Core;
+
+namespace VallezHotels
+{
+    public partial class FrmDescricaoQuarto : Form
+    {
+
+        public int NumeroQuarto;
+        public int StatusQuarto;
+
+        public FrmDescricaoQuarto()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDescricaoQuarto_Load(object sender, EventArgs e)
+        {
+            lblNumQuarto.Text = $"Quarto: {this.NumeroQuarto}";
+            this.Cursor = Cursors.Hand ;
+
+            switch (StatusQuarto)
+            {
+                case 1:
+                    this.BackColor = Color.FromArgb(0, 173, 181);
+                    break;
+                case 2:
+                    this.BackColor = Color.FromArgb(235, 188, 61);
+                    break;
+                case 3:
+                    this.BackColor = Color.FromArgb(255, 131, 3);
+                    break;
+                case 4:
+                    this.BackColor = Color.FromArgb(251, 54, 64);
+                    break;
+            }
+
+
+        }
+
+        private void FrmDescricaoQuarto_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void FrmDescricaoQuarto_MouseClick(object sender, MouseEventArgs e)
+        {
+            FrmLocacao locacao = new FrmLocacao();
+            Helper.StartForm(locacao, null);
+        }
+    }
+}
