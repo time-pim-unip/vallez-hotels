89757e9d96bd4f0fe86440869041b4c9da674064
dcmunhoz <dc.munhoz@hotmail.com>
2021-07-13 20:37:46 -0300

    Implementando interfaces geral de conexão com o banco.

---
+++ VallezHotels/VallezHotels/Source/DB/Interfaces/IDBComandosBasicosEntidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VallezHotels.Source.DB.Interfaces
+{
+    public interface IDBComandosBasicosEntidade<T>
+    {
+        void Inserir();
+        T BuscarPeloID(int id);
+        List<T> BuscarTodos();
+        T Atualizar(T obj);
+        void Deletar(T obj);
+
+    }
+}
