408beae8e269c2b4ddb512e5233ad2589f7b3c2f
dcmunhoz <dc.munhoz@hotmail.com>
2021-07-14 21:26:24 -0300

    Implementado serviço de inserir usuário no banco de dados

---
+++ VallezHotels/VallezHotels/Source/DB/UsuarioDB.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VallezHotels.Source.DB.Interfaces;
+using VallezHotels.Source.Entidades;
+
+namespace VallezHotels.Source.DB
+{
+    public class UsuarioDB : IDBComandosBasicosEntidade<Usuario>
+    {
+
+        private readonly IDBConexao _conn;
+
+        public UsuarioDB(IDBConexao conn)
+        {
+            _conn = conn;
+        }
+
+        public Usuario Atualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario BuscarPeloID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Inserir(Usuario usuario)
+        {
+           
+            try
+            {
+                using (var conn = _conn.Conexao())
+                {
+
+                    conn.Open();
+
+                    using (var insert = conn.CreateCommand())
+                    {
+
+                        insert.CommandText = "INSERT INTO vallez.usuarios (uuid_usuario, usuario, senha, tipo_usuario, status) VALUES(vallez.uuid_generate_v4(), @USUARIO, @SENHA, @TIPO_USUARIO, @STATUS) returning *; ";
+                        insert.AddParameter("@USUARIO", usuario.NomeUsuario);
+                        insert.AddParameter(":SENHA", usuario.Senha, DbType.String );
+                        insert.AddParameter("@TIPO_USUARIO", usuario.TipoUsuario);
+                        insert.AddParameter("@STATUS", usuario.Status);
+                        
+                        var reader = insert.ExecuteReader();
+
+                        Usuario u = null;
+                        while(reader.Read())
+                        {
+                            u = new Usuario();
+                            u.Id = int.Parse(reader["id_usuario"].ToString());
+                            u.Uuid = reader["uuid_usuario"].ToString();
+                            u.NomeUsuario = reader["usuario"].ToString();
+                            u.Senha = reader["senha"].ToString();
+                            u.TipoUsuario = reader["tipo_usuario"].ToString();
+                            u.Status = bool.Parse(reader["status"].ToString());
+                            u.CreatedAt = DateTime.Parse(reader["created_at"].ToString());
+                            u.UpdatedAt = DateTime.Parse(reader["updated_at"].ToString());
+                            
+                        }
+
+                        return u;
+                    }
+
+                }
+
+            }
+            catch (Exception e )
+            {
+                throw new Exception("REPOSITORY: " + e.Message);
+            }
+        }
+
+    }
+}
---
+++ VallezHotels/VallezHotels/Source/Servicos/UsuarioServico.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VallezHotels.Source.DB;
+using VallezHotels.Source.DB.Conexao;
+using VallezHotels.Source.Entidades;
+
+namespace VallezHotels.Source.Servicos
+{
+    public class UsuarioServico
+    {
+
+        private readonly UsuarioDB _db;
+
+        public UsuarioServico()
+        {
+
+            _db = new UsuarioDB(new PGConexao());
+
+        }
+
+        public Usuario InserirUsuario(Usuario usuario)
+        {
+            try
+            {
+                Usuario u = _db.Inserir(usuario);
+                return u;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("SERVICE: " + e.Message);
+            }
+        }
+
+    }
+}
