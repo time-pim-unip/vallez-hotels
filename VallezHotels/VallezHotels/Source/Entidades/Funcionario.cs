35f3ee5f440b8a524f140b8e5680d20ead7f9747
dcmunhoz <dc.munhoz@hotmail.com>
2021-07-17 21:36:12 -0300

    Teste: Implementando testes em PessoaServico

---
+++ VallezHotels/VallezHotels/Source/Entidades/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VallezHotels.Source.Entidades
+{
+    public class Funcionario : Pessoa
+    {
+
+        public int IdFuncionario { get; set; }
+        public string UuidFuncionario { get; set; }
+        //public Pessoa Pessoa { get; set; }
+        public Usuario Usuario { get; set; }
+        public string CTPS { get; set; }
+        public DateTime Admissao { get; set; }
+        public DateTime CreatedAtFuncionario { get; set; }
+        public DateTime UpdatedAtFuncionario { get; set; }
+
+        public Funcionario()
+        {
+            CreatedAtFuncionario = new DateTime();
+            UpdatedAtFuncionario = new DateTime();
+        }
+
+        public Funcionario(int id, string uuid, Usuario usuario, string cTPS, DateTime admissao)
+        {
+            IdFuncionario = id;
+            UuidFuncionario = uuid;
+            //Pessoa = pessoa;
+            Usuario = usuario;
+            CTPS = cTPS;
+            Admissao = admissao;
+            CreatedAtFuncionario = new DateTime();
+            UpdatedAtFuncionario = new DateTime();
+        }
+    }
+}
---
+++ VallezHotels/VallezHotels/Source/Entidades/Hospede.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VallezHotels.Source.Entidades
+{
+    class Hospede : Pessoa
+    {
+
+        public int Id { get; set; }
+        public string Uuid { get; set; }
+        public Pessoa Pessoa { get; set; }
+        public Usuario Usuario { get; set; }
+        public bool Consentimento { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Hospede()
+        {
+            CreatedAt = new DateTime();
+            UpdatedAt = new DateTime();
+        }
+
+        public Hospede(int id, string uuid, Pessoa pessoa, Usuario usuario, bool consentimento, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            Uuid = uuid;
+            Pessoa = pessoa;
+            Usuario = usuario;
+            Consentimento = consentimento;
+            CreatedAt = new DateTime();
+            UpdatedAt = new DateTime();
+        }
+    }
+}
