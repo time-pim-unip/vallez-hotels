33683d1aa92a055c645adbd7a24cbe4cce57a767
dcmunhoz <dc.munhoz@hotmail.com>
2021-08-01 21:30:53 -0300

    Formulário de listagem genérica

--- VallezHotels/VallezHotels/Forms/FrmListagemGenerica.Designer.cs
+++ VallezHotels/VallezHotels/Forms/FrmListagemGenerica.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace VallezHotels.Forms
+namespace VallezHotels
 {
     partial class FrmListagemGenerica
     {
@@ -29,22 +29,104 @@ namespace VallezHotels.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgListagem = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgListagem)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtDescricao);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 63);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pesquisa";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDescricao.Location = new System.Drawing.Point(9, 32);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(287, 20);
+            this.txtDescricao.TabIndex = 1;
+            this.txtDescricao.TextChanged += new System.EventHandler(this.txtDescricao_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descrição";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.dgListagem);
+            this.groupBox2.Location = new System.Drawing.Point(12, 81);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(302, 417);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultado";
+            // 
+            // dgListagem
+            // 
+            this.dgListagem.AllowUserToAddRows = false;
+            this.dgListagem.AllowUserToDeleteRows = false;
+            this.dgListagem.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgListagem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgListagem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgListagem.Location = new System.Drawing.Point(3, 16);
+            this.dgListagem.Name = "dgListagem";
+            this.dgListagem.ReadOnly = true;
+            this.dgListagem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgListagem.Size = new System.Drawing.Size(296, 398);
+            this.dgListagem.TabIndex = 0;
+            this.dgListagem.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgListagem_CellDoubleClick);
             // 
             // FrmListagemGenerica
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(358, 510);
+            this.ClientSize = new System.Drawing.Size(326, 510);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "FrmListagemGenerica";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Listagem Genérica";
+            this.Load += new System.EventHandler(this.FrmListagemGenerica_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgListagem)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgListagem;
     }
 }
--- VallezHotels/VallezHotels/Forms/FrmListagemGenerica.cs
+++ VallezHotels/VallezHotels/Forms/FrmListagemGenerica.cs
@@ -8,13 +8,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace VallezHotels.Forms
+using VallezHotels.Source.DTO;
+
+namespace VallezHotels
 {
     public partial class FrmListagemGenerica : Form
     {
+        public List<ListagemGenericaDTO> Lista;
+        public int CodigoSelecionado;
+
         public FrmListagemGenerica()
         {
             InitializeComponent();
+        }
+
+        private void FrmListagemGenerica_Load(object sender, EventArgs e)
+        {
+            dgListagem.DataSource = Lista;
+        }
+
+        private void txtDescricao_TextChanged(object sender, EventArgs e)
+        {
+            var listagem = from l in Lista
+                           where l.Descricao.ToUpper().Contains(txtDescricao.Text.Trim().ToUpper().ToString())
+                           select l;
+
+            dgListagem.DataSource = listagem.ToList();
+
+        }
+
+        private void dgListagem_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            this.CodigoSelecionado = int.Parse(dgListagem.Rows[e.RowIndex].Cells["Codigo"].Value.ToString());
+
+            this.Close();
         }
     }
 }
